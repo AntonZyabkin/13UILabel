55616568e6e93e657ed580d330c42d0e5bea04bb
AntonZyabkin <zyabkinantonv@gmail.com>
2022-05-27 12:21:31 +0300

    27 may... tired

---
+++ TSD 13.1 UITextField/EnterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EnterViewController.swift
+//  TSD 13.1 UITextField
+//
+//  Created by Anton Zyabkin on 27.05.2022.
+//
+
+import UIKit
+
+class EnterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TSD 13.1 UITextField/enterVC.swift
@@ -0,0 +1,8 @@
+//
+//  enterVC.swift
+//  TSD 13.1 UITextField
+//
+//  Created by Anton Zyabkin on 27.05.2022.
+//
+
+import Foundation
